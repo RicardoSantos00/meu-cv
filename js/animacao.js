bc443bf6aee6de68933f0bc8969af833d586af9d
ricardo.santos <ricardo.santos@NOTE053166>
2020-05-06 21:15:54 -0300

    animação da pagina com scroll e começando a fazer gráfico de skills

---
+++ js/animacao.js
@@ -0,0 +1,37 @@
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + 600; //((window.innerHeight * 3) / 4);
+    target.forEach(function(elementoTela) {
+        if ((windowTop) > elementoTela.offsetTop) {
+            elementoTela.classList.add(animationClass);
+        } else {
+            elementoTela.classList.remove(animationClass);
+        }
+    });
+
+}
+
+animeScroll();
+
+if (target.length)
+    window.addEventListener('scroll', debounce(function() {
+        animeScroll();
+    }, 100));
